6f9d5413f024c981fbbe9a16b79c35ee319cd109
weiowyw <haxjlebatop@gmail.com>
2024-09-11 23:18:13 +0500

    No fetching fix

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -6,10 +6,13 @@ const Items = ({items, onAdd, onShowItem}) => {
     return (
         <div className='items_list'>
             <div className='header_fill'></div>
-                {items.map(item => (
-                    <Item item={item} onAdd={onAdd} onShowItem={onShowItem} key={item.id}/>
+            {items.length === 0 ? <p>
+                If you are seeing this message, most likely https://fakestoreapi.com/products servers are down and unable to provide all the products.
+            </p> : items.map(item => (
+                        <Item item={item} onAdd={onAdd} onShowItem={onShowItem} key={item.id}/>
                     )
                 )}
+
         </div>
     );
 };
